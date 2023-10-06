31b3d862a2e458d17cf3bcf6500cdae90a2ae077
Anderson Shen <143461521+andshen-github@users.noreply.github.com>
2023-10-06 13:09:12 +0800

    Update config.py

--- backend/common/config.py
+++ backend/common/config.py
@@ -1,6 +1,4 @@
-# Claudeに渡すパラメータの設定をします。
-# ご利用のアプリケーションに合わせて調整してください。
-# 参考: https://docs.anthropic.com/claude/reference/complete_post
+# Reference: https://docs.anthropic.com/claude/reference/complete_post
 GENERATION_CONFIG = {
     "max_tokens_to_sample": 500,
     "temperature": 0.0,
